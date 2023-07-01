6073d8e65447f86619e1ad53675ff6c367c0de5b
Aaron <arnhsmith@gmail.com>
2023-07-01 16:18:34 -0400

    code checked and passed all tests

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,17 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
+  // the line below adds a display to the console of the html elements that looks like this
+  // console.log
+  //     <body>
+  //       <div>
+  //         <div>
+  //           please pass this test
+  //         </div>
+  //       </div>
+  //     </body>
+
+  screen.debug();
+
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
